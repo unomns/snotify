ca97ed0cd59294e9a3890220b66693b65ebc54b2
unomns <unomns@gmail.com>
2025-06-27 18:54:27 +0800

    clean config

--- config.py
+++ config.py
@@ -3,9 +3,6 @@ import os
 
 load_dotenv()
 
-STOCK_URL = os.getenv("STOCK_URL", "https://")
-STOCK_PORT = os.getenv("STOCK_PORT", "80")
-STOCK_API_KEY = os.getenv("STOCK_API_KEY", "")
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID", "")
 POLL_INTERVAL_SECONDS = int(os.getenv("POLL_INTERVAL_SECONDS", "60"))
